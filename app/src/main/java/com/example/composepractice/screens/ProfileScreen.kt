9b179aa4029171e69e1bca839feb3457d5ec2f45
Numan Ali <flopy6663@gmail.com>
2024-06-06 11:57:13 +0500

    ParallaxSwiper

--- app/src/main/java/com/example/composepractice/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/composepractice/screens/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.example.composepractice.screens
 
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -26,10 +27,11 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
 import androidx.compose.ui.util.lerp
+import coil.compose.AsyncImage
 import com.example.composepractice.R
 import com.example.composepractice.ui.theme.ComposePracticeTheme
+import java.net.URI
 
 @Composable
 fun ProfileScreen() {
@@ -65,60 +67,84 @@ fun ProfileScreen() {
 @Composable
 fun ParallaxSwiper(
     viewPortFraction: Float = 0.85f,
-    backgroundZoomEnabled: Boolean = true,
-    foregroundFadeEnabled: Boolean = true,
     initialPage: Int = 0,
     initialPageOffsetFraction: Float = 0f
 ) {
     val images = listOf(
-        painterResource(id = R.drawable.image1),
-        painterResource(id = R.drawable.image2),
-        painterResource(id = R.drawable.image3),
-        painterResource(id = R.drawable.image4),
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4,
+        R.drawable.image5
     )
+
     val pagerState = rememberPagerState(
-        pageCount = { images.size },
         initialPage = initialPage,
+        pageCount = { images.size },
         initialPageOffsetFraction = initialPageOffsetFraction
     )
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val contentPadding = (screenWidth * (1f - viewPortFraction) / 2)
+
     HorizontalPager(
         state = pagerState,
         modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(horizontal = (1f - viewPortFraction) * .5f * LocalConfiguration.current.screenWidthDp.dp),
+        contentPadding = PaddingValues(horizontal = contentPadding)
     ) { page ->
         val pageOffset = calculateCurrentOffsetForPage(page, pagerState)
-        Card(
+        SwiperItem(
+            image = images[page],
+            pageOffset = pageOffset
+        )
+    }
+}
+
+@Composable
+fun SwiperItem(
+    image: Int,
+    pageOffset: Float,
+) {
+
+    val translationFraction = if (pageOffset >= 0) {
+        pageOffset * 0.3f // Swipe from left to right
+    } else {
+        pageOffset * -0.3f // Swipe from right to left
+    }
+
+    val scale = lerp(
+        start = 3.0f, // this can be adjusted to change the scaling of the image
+        stop = 1f,
+        fraction = 1f - translationFraction.coerceIn(0f, 1f)
+    )
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .aspectRatio(1.4f)
+    ) {
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1.8f)
-                .graphicsLayer {
-                    if (backgroundZoomEnabled) {
-                        val scale = lerp(
-                            start = 0.85f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        )
+                .fillMaxSize()
+                .clip(MaterialTheme.shapes.large)
+        ) {
+
+            AsyncImage(
+                model = image,
+                contentDescription = "Scene image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
                         scaleX = scale
                         scaleY = scale
-                    }
-                    if (foregroundFadeEnabled) {
-                        alpha = lerp(
-                            start = 0.5f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        )
-                    }
-                }
-        ) {
-            Image(
-                painter = images[page],
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
+                        translationX = translationFraction * size.width * 0.3f
+                    },
                 contentScale = ContentScale.Crop
             )
         }
     }
 }
+
 
 @Composable
 fun calculateCurrentOffsetForPage(page: Int, pagerState: PagerState): Float {
